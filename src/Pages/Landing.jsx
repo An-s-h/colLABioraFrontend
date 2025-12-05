00c48b5e10962e7df14ef5b25fe943683cc8128d
Anshh <anshchachra0@gmail.com>
2025-12-05 21:30:30 +0530

    changed a line

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -550,7 +550,7 @@ export default function Landing() {
                                 className="text-xs sm:text-sm font-semibold"
                                 style={{ color: "#2F3C96" }}
                               >
-                                Search using 6 free searches
+                               You have 6 free searches left
                               </span>
                             </div>
                           </div>
